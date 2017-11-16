514037c4e8f100ac7445d5ae61e87877699d9e2a
Thomas Baier <thomas.baier@luminis.eu>
2017-11-16 23:28:10 +0100

    Use composition instead of inheriting from list and make containers unmodifiable

--- midiio/containers.py
+++ midiio/containers.py
@@ -1,71 +1,63 @@
 from pprint import pformat
 
-class Pattern(list):
-    def __init__(self, tracks=[], resolution=220, format=1, tick_relative=True):
-        self.format = format
-        self.resolution = resolution
-        self.tick_relative = tick_relative
-        super().__init__(tracks)
+class Pattern(object):
+    def __init__(self, tracks=[], resolution=220, format=1):
+        self._format = format
+        self._resolution = resolution
+        self._tracks = tuple(tracks)
+
+    @property
+    def format(self):
+        return self._format
+    @property
+    def resolution(self):
+        return self._resolution
+    @property
+    def tracks(self):
+        return self._tracks
+
+    def append(track):
+        return Pattern(self._format, self.resolution, self._tracks + (track, ))
+
+    def extend(tracks):
+        return Pattern(self._format, self.resolution, self._tracks + tuple(tracks))
+
+    def __getitem__(self, key):
+        return self._tracks[key]
+
+    def __iter__(self):
+        return iter(self._tracks)
+
+    def __len__(self):
+        return len(self._tracks)
 
     def __repr__(self):
-        return "midi.Pattern(format=%r, resolution=%r, tracks=\\\n%s)" % \
-            (self.format, self.resolution, pformat(list(self)))
+        return "midiio.Pattern(format=%r, resolution=%r, tracks=\\\n%s)" % \
+            (self.format, self.resolution, pformat(list(self._tracks)))
 
-    def make_ticks_abs(self):
-        self.tick_relative = False
-        for track in self:
-            track.make_ticks_abs()
 
-    def make_ticks_rel(self):
-        self.tick_relative = True
-        for track in self:
-            track.make_ticks_rel()
+class Track(object):
+    def __init__(self, events=[]):
+        self._events = tuple(events)
 
-    def __getitem__(self, item):
-        if isinstance(item, slice):
-            indices = item.indices(len(self))
-            return Pattern(resolution=self.resolution, format=self.format,
-                            tracks=(super().__getitem__(i) for i in range(*indices)))
-        else:
-            return super().__getitem__(item)
+    @property
+    def events(self):
+        return self._events
 
-    def __getslice__(self, i, j):
-        # The deprecated __getslice__ is still called when subclassing built-in types
-        # for calls of the form List[i:j]
-        return self.__getitem__(slice(i, j))
+    def append(event):
+        return Track(self._events + (event, ))
 
-class Track(list):
-    def __init__(self, events=[], tick_relative=True):
-        self.tick_relative = tick_relative
-        super().__init__(events)
+    def extend(events):
+        return Track(self._events + tuple(events))
 
-    def make_ticks_abs(self):
-        if (self.tick_relative):
-            self.tick_relative = False
-            running_tick = 0
-            for event in self:
-                event.tick += running_tick
-                running_tick = event.tick
+    def __getitem__(self, key):
+        return self._events[key]
 
-    def make_ticks_rel(self):
-        if (not self.tick_relative):
-            self.tick_relative = True
-            running_tick = 0
-            for event in self:
-                event.tick -= running_tick
-                running_tick += event.tick
+    def __iter__(self):
+        return iter(self._events)
 
-    def __getitem__(self, item):
-        if isinstance(item, slice):
-            indices = item.indices(len(self))
-            return Track((super().__getitem__(i) for i in range(*indices)))
-        else:
-            return super().__getitem__(item)
-
-    def __getslice__(self, i, j):
-        # The deprecated __getslice__ is still called when subclassing built-in types
-        # for calls of the form List[i:j]
-        return self.__getitem__(slice(i, j))
+    def __len__(self):
+        return len(self._events)
 
     def __repr__(self):
-        return "midi.Track(\\\n  %s)" % (pformat(list(self)).replace('\n', '\n  '), )
+        return "midiio.Track(\\\n  %s)" % (pformat(list(self._events)).replace('\n', '\n  '), )
